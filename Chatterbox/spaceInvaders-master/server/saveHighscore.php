25767a517bc1cac3435f785f89f3ac7cb4674899
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-10-08 00:29:29 +0500

    Delete Space Invaders game
    Archives is still saved tho

--- Chatterbox/spaceInvaders-master/server/saveHighscore.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if (isset($_POST["data"])) {
-        $file = "../public/highscores/highscores.json";
-        $fh = fopen($file, 'w') or die("can't open file");
-        $stringData = $_POST["data"];
-        fwrite($fh, $stringData);
-        fclose($fh);
-    }
-?>
